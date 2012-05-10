96f46d0b59e6585367b931ea5153dec71500790b
Gaetan Muller <m.gaetan89@gmail.com>
2012-05-10 11:49:11 +0200

    Had PM notification in top navigation bar

--- src/Euro/PrivateMessageBundle/Controller/PrivateMessageController.php
+++ src/Euro/PrivateMessageBundle/Controller/PrivateMessageController.php
@@ -2,12 +2,27 @@
 
 namespace Euro\PrivateMessageBundle\Controller;
 
+use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PrivateMessageController extends Controller {
 
-	public function indexAction($name) {
-		return $this->render('EuroPrivateMessageBundle:PrivateMessage:index.html.twig', array('name' => $name));
+	public function hasNewMessageAction() {
+		$user = $this->getUser();
+		if (!$user instanceof UserInterface) {
+			throw new \Exception('You are not allowed to access this page !');
+		}
+
+		$em = $this->getDoctrine()->getEntityManager();
+		$new_pm = $em->getRepository('EuroPrivateMessageBundle:PrivateMessage')->getNewMessageCount($user);
+
+		return $this->render('EuroPrivateMessageBundle:PrivateMessage:has_new_message.html.twig', array(
+					'new_pm' => $new_pm,
+				));
+	}
+
+	private function getUser() {
+		return $this->get('security.context')->getToken()->getUser();
 	}
 
 }
---
+++ src/Euro/PrivateMessageBundle/Entity/PrivateMessage.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Euro\PrivateMessageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Euro\PrivateMessageBundle\Entity\PrivateMessage
+ *
+ * @ORM\Table(name="private_message")
+ * @ORM\Entity(repositoryClass="Euro\PrivateMessageBundle\Entity\PrivateMessageRepository")
+ */
+class PrivateMessage {
+	/**
+	 * @var integer $id
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+	/**
+	 * @var string $conversation
+	 *
+	 * @ORM\Column(name="conversation", type="string", length=16)
+	 */
+	private $conversation;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Euro\UserBundle\Entity\User")
+	 * @ORM\JoinColumn(name="from_user_id", referencedColumnName="id")
+	 */
+	private $from_user;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Euro\UserBundle\Entity\User")
+	 * @ORM\JoinColumn(name="to_user_id", referencedColumnName="id")
+	 */
+	private $to_user;
+
+	/**
+	 * @var datetime $post_date
+	 *
+	 * @ORM\Column(name="post_date", type="datetime")
+	 */
+	private $post_date;
+
+	/**
+	 * @var string $title
+	 *
+	 * @ORM\Column(name="title", type="string", length=255)
+	 */
+	private $title;
+
+	/**
+	 * @var text $text
+	 *
+	 * @ORM\Column(name="text", type="text")
+	 */
+	private $text;
+
+	/**
+	 * @var boolean $is_read
+	 *
+	 * @ORM\Column(name="is_read", type="boolean")
+	 */
+	private $is_read;
+
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * Set conversation
+	 *
+	 * @param string $conversation
+	 */
+	public function setConversation($conversation) {
+		$this->conversation = $conversation;
+	}
+
+	/**
+	 * Get conversation
+	 *
+	 * @return string
+	 */
+	public function getConversation() {
+		return $this->conversation;
+	}
+
+	/**
+	 * Set from_user
+	 *
+	 * @param Euro\UserBundle\Entity\User $from_user
+	 */
+	public function setFromUser(\Euro\UserBundle\Entity\User $from_user) {
+		$this->from_user = $from_user;
+	}
+
+	/**
+	 * Get from_user
+	 *
+	 * @return Euro\UserBundle\Entity\User
+	 */
+	public function getFromUser() {
+		return $this->from_user;
+	}
+
+	/**
+	 * Set to_user
+	 *
+	 * @param Euro\UserBundle\Entity\User $to_user
+	 */
+	public function setToUser(\Euro\UserBundle\Entity\User $to_user) {
+		$this->to_user = $to_user;
+	}
+
+	/**
+	 * Get to_user
+	 *
+	 * @return Euro\UserBundle\Entity\User
+	 */
+	public function getToUser() {
+		return $this->to_user;
+	}
+
+	/**
+	 * Set post_date
+	 *
+	 * @param datetime $postDate
+	 */
+	public function setPostDate($postDate) {
+		$this->post_date = $postDate;
+	}
+
+	/**
+	 * Get post_date
+	 *
+	 * @return datetime
+	 */
+	public function getPostDate() {
+		return $this->post_date;
+	}
+
+	/**
+	 * Set title
+	 *
+	 * @param string $title
+	 */
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+	/**
+	 * Get title
+	 *
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/**
+	 * Set text
+	 *
+	 * @param text $text
+	 */
+	public function setText($text) {
+		$this->text = $text;
+	}
+
+	/**
+	 * Get text
+	 *
+	 * @return text
+	 */
+	public function getText() {
+		return $this->text;
+	}
+
+	/**
+	 * Set is_read
+	 *
+	 * @param boolean $isRead
+	 */
+	public function setIsRead($isRead) {
+		$this->is_read = $isRead;
+	}
+
+	/**
+	 * Get is_read
+	 *
+	 * @return boolean
+	 */
+	public function getIsRead() {
+		return $this->is_read;
+	}
+
+}
