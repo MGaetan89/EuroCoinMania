64f477543e6fa69bf68839b5f32e8a233a4ec909
MGaetan89 <m.gaetan89@gmail.com>
2012-09-03 19:49:03 +0200

    Enable closing PM

--- src/Euro/PrivateMessageBundle/Controller/PrivateMessageController.php
+++ src/Euro/PrivateMessageBundle/Controller/PrivateMessageController.php
@@ -7,6 +7,56 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PrivateMessageController extends Controller {
 
+	public function closeAction($id) {
+		$flashBag = $this->get('session')->getFlashBag();
+		if (!$user = $this->getUser()) {
+			$flashBag->add('error', 'user.login_required');
+
+			return $this->redirect($this->generateUrl('fos_user_security_login'));
+		}
+
+		$doctrine = $this->getDoctrine();
+		$conversation = $doctrine->getRepository('EuroPrivateMessageBundle:Conversation')->find($id);
+
+		if (!$conversation) {
+			$flashBag->add('error', 'pm.not_found');
+
+			return $this->redirect($this->generateUrl('pm_list'));
+		}
+
+		if ($conversation->getFromUser()->getId() !== $user->getId() && $conversation->getToUser()->getId() !== $user->getId()) {
+			$flashBag->add('error', 'pm.not_for_you');
+
+			return $this->redirect($this->generateUrl('pm_list'));
+		}
+
+		$message = new Message();
+		$message->setContent('pm.text.conversation_closed');
+		$message->setType(Message::TYPE_INFO);
+		$message->setConversation($conversation);
+
+		if ($conversation->getFromUser()->getId() === $user->getId()) {
+			$message->setDirection(Message::DIRECTION_FROM_TO);
+		} else {
+			$message->setDirection(Message::DIRECTION_TO_FROM);
+		}
+
+		$em = $doctrine->getManager();
+		$em->persist($message);
+
+		$conversation->setOpen(false);
+		$conversation->addMessage($message);
+
+		$em->flush();
+
+		$flashBag->add('success', 'pm.closed_success');
+
+		return $this->redirect($this->generateUrl('pm_read', array(
+							'id' => $conversation->getId(),
+							'title' => $this->get('translator')->trans($conversation->getTitle()),
+						)));
+	}
+
 	public function listAction($archives) {
 		$flashBag = $this->get('session')->getFlashBag();
 		if (!$user = $this->getUser()) {
