9a3c9d7acbe7c4eb83c2491b1d34aa2de1ef181e
MGaetan89 <m.gaetan89@gmail.com>
2012-10-04 20:25:25 +0200

    Finish implementing the EuroContactBundle

--- src/Euro/ContactBundle/Controller/ContactController.php
+++ src/Euro/ContactBundle/Controller/ContactController.php
@@ -4,13 +4,43 @@ namespace Euro\ContactBundle\Controller;
 
 use Euro\ContactBundle\Form\Type\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller {
 
-	public function contactAction() {
-		$form = $this->createForm(new ContactType());
+	public function contactAction(Request $request) {
+		$default = array(
+			'email' => null,
+			'message' => null,
+			'name' => null,
+			'subject' => 'other',
+		);
+		$form = $this->createForm(new ContactType(), $default);
 
-		return $this->render('EuroContactBundle:Contact:index.html.twig', array(
+		if ($request->isMethod('POST')) {
+			$form->bind($request);
+			$data = $form->getData();
+			$translator = $this->get('translator');
+
+			$subject = 'contact.subject.' . $data['subject'];
+			if ($translator->trans($subject) !== $subject) {
+				$subject = $translator->trans($subject);
+			}
+
+			$message = \Swift_Message::newInstance()
+					->setSubject($subject)
+					->setFrom($data['email'])
+					->setTo('contact@eurocoin-mania.eu')
+					->setBody($data['message']);
+
+			if ($this->get('mailer')->send($message)) {
+				$this->get('session')->getFlashBag()->add('success', 'contact.send.success');
+			} else {
+				$this->get('session')->getFlashBag()->add('error', 'contact.send.fail');
+			}
+		}
+
+		return $this->render('EuroContactBundle:Contact:contact.html.twig', array(
 					'form' => $form->createView(),
 				));
 	}
