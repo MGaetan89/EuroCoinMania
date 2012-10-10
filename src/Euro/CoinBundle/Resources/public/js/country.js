8a43d84d3fb6acb5b8cd28fe2d65ec40fe3fea0e
MGaetan89 <m.gaetan89@gmail.com>
2012-10-10 17:01:26 +0200

    Update Country map

---
+++ src/Euro/CoinBundle/Resources/public/js/country.js
@@ -0,0 +1,40 @@
+$(function () {
+	var color = {
+		from: '#0044CC',
+		to: '#006DCC'
+	}, map = $('#map'), countries = map.data('countries');
+
+	$.get(map.attr('src'), function (data) {
+		var svg = $(data).find('svg').attr('id', map.attr('id'));
+
+		map.replaceWith(svg);
+		map = $('#map');
+
+		$('table tbody').on('mouseenter', 'tr', function () {
+			var id = $(this).attr('id').substr(0, 2);
+
+			$('#' + id).css('fill', color.from);
+		}).on('mouseleave', 'tr', function () {
+			var id = $(this).attr('id').substr(0, 2);
+
+			$('#' + id).css('fill', color.to);
+		});
+
+		function hoverIn() {
+			$('#' + this.id + '-list').addClass('success');
+		}
+
+		function hoverOut() {
+			$('#' + this.id + '-list').removeClass('success');
+		}
+
+		$.each(countries, function (index, country) {
+			map.find('#' + country.nameiso)
+			.attr('title', country.name)
+			.hover(hoverIn, hoverOut)
+			.on('click', function () {
+				location.href = country.path;
+			});
+		});
+	});
+});
