ed2a1e2fdc6b7d1e0cc2881b466e71d745d13a9f
Gaëtan Muller <m.gaetan89@gmail.com>
2012-12-02 20:37:52 +0100

    Always display the footer menu

--- src/Euro/CoinBundle/Resources/public/js/app.js
+++ src/Euro/CoinBundle/Resources/public/js/app.js
@@ -1,16 +1,9 @@
 $(function () {
-	var footer_indicator = $('#footer-toggle').find('i'), footer_short = $('#footer-short');
-
 	$('body').popover({
 		selector: '[rel=popover]',
 		template: '<div class="popover"><div class="close" data-dismiss="alert">&times;</div><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>'
 	}).tooltip({
 		selector: '[rel=tooltip]'
-	});
-
-	$('#footer-extend').on('show hide', function () {
-		footer_indicator.toggleClass('icon-minus icon-plus');
-		footer_short.toggle();
 	});
 });
 
