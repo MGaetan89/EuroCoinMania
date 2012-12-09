146e3c3a6a377898c14fececea2e0bd7d222677d
Gaëtan Muller <m.gaetan89@gmail.com>
2012-12-09 11:18:20 +0100

    Update to Bootstrap 2.2.2

--- src/Euro/CoinBundle/Resources/public/js/app.js
+++ src/Euro/CoinBundle/Resources/public/js/app.js
@@ -1,7 +1,7 @@
 $(function () {
 	$('body').popover({
 		selector: '[data-hover=popover]',
-		template: '<div class="popover"><div class="close" data-dismiss="alert">&times;</div><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>'
+		template: '<div class="popover"><div class="close" data-dismiss="alert">&times;</div><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"></div></div></div>'
 	}).tooltip({
 		selector: '[data-hover=tooltip]'
 	});
