7fd2325fa615e3269a6ac6c0e08ae28d48f557fc
Gaëtan Muller <m.gaetan89@gmail.com>
2013-01-05 14:43:14 +0100

    Update vendors

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit7c3e6e4845c0535a48b5b96c77fcc56f::getLoader();
+return ComposerAutoloaderInit787299697e65d423dd6a5ae611ad8972::getLoader();
