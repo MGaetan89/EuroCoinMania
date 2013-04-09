1c436d8964c2f7550081bd37380f9fecfef60788
Gaëtan Muller <m.gaetan89@gmail.com>
2013-04-09 21:43:45 +0200

    Closes #57 : Update to jQuery 1.9.1

--- src/Euro/CoinBundle/Resources/public/js/app.js
+++ src/Euro/CoinBundle/Resources/public/js/app.js
@@ -1,10 +1,10 @@
 $(function () {
 	$('body').popover({
+		html: true,
 		selector: '[data-hover=popover]',
-		template: '<div class="popover"><div class="close" data-dismiss="alert">&times;</div><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"></div></div></div>'
+		template: '<div class="popover"><div class="close" data-dismiss="alert">&times;</div><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
 	}).tooltip({
 		selector: '[data-hover=tooltip]'
 	});
-
-	$('#stats-carousel').carousel();
 });
+
